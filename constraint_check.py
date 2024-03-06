3567b78cfc74817b3adbccc2a9f5c0b8fb677522
Ethan Jansen <ethan.jansen@wallawalla.edu>
2024-03-06 12:51:31 -0800

    constraint check finalized

--- constraint_check.py
+++ constraint_check.py
@@ -11,11 +11,12 @@ file = "QAP_PSD_SolverOut.csv"
 # parse data from dat file
 val = pd.read_csv(file, sep=",", header=None, nrows=1)[0][0]
 df = pd.read_csv(file, sep=",", header=None, skiprows=1)
-Y = df.to_numpy(dtype=np.float32)
+Y = df.to_numpy(dtype=np.float64)
 m = Y.shape[0]
 n = int(np.sqrt(m-1))
 
 ###################### Setup ##############################
+tol = 1e-6
 
 I = np.identity(n) # identity
 E = np.ones(shape=(n,n)) # ones matrix
@@ -53,31 +54,38 @@ def o0(Y):
 
 # Y[0][0] == 1
 print("Y[0][0] == 1: ", Y[0][0])
-print(mt.isclose(Y[0][0], 1, rel_tol=1e-6))
+print(mt.isclose(Y[0][0], 1, rel_tol=tol))
 
 # Diag(Y) == Y[:,0]
 print("Diag(Y) == Y[:,0]:")
-print(np.isclose(Y[:,0], Y.diagonal, rtol=1e-6))
+print(np.allclose(Y[:,0],  np.diag(Y), rtol=tol))
 
 # Trace(D*Y) == 0
-print("Trace(D*Y) == 0:")
-print(mt.isclose(np.trace(np.matmul(D, Y)), 0, rel_tol=1e-6))
+t = np.trace(np.matmul(D, Y))
+print("Trace(D*Y) == 0:", t)
+print(mt.isclose(t, 0, rel_tol=tol, abs_tol=tol)) # need to use abs_tol for near 0
 
 # b0(Y) == I
 print("b0(Y) == I:")
-print(np.isclose(b0(Y), I, rtol=1e-6))
+print(np.allclose(b0(Y), I, rtol=tol, atol=tol))
 
 # o0(Y) == I
 print("o0(Y) == I:")
-print(np.isclose(o0(Y), I, rtol=1e-6))
+print(np.allclose(o0(Y), I, rtol=tol, atol=tol))
 
 # Y is PSD
-print("Y is PSD:")
+print("Y is symmetric:")
+sym = np.allclose(Y, Y.T, rtol=tol, atol=tol)
+print(sym)
+print("Y has all nonnegative eignevalues:")
+pos_eigs = False
 try:
-    np.linalg.cholesky(Y)
-except LinAlgError:
+    eigs = np.linalg.eigvals(Y)
+    pos_eigs = np.all(eigs >= -tol)
+    print(pos_eigs)
+except np.linalg.LinAlgError as e:
+    print(repr(e))
     print(False)
-else:
-    print(True)
-finally:
-    print("Done")
+
+print("Y is PSD:")
+print(sym and pos_eigs)
