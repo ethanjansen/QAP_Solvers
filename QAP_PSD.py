1b412c260ca630b21f7228a33c57ed6f75762c94
Ethan Jansen <ethan.jansen@wallawalla.edu>
2024-03-06 09:43:05 -0800

    psd solver

---
+++ QAP_PSD.py
@@ -0,0 +1,51 @@
+############# Get QAP from QAPLIB #####################\n",
+import cvxpy as cp
+import numpy as np
+import pandas as pd
+import sys
+np.set_printoptions(threshold=sys.maxsize)
+
+link = "https://coral.ise.lehigh.edu/wp-content/uploads/2014/07/data.d/esc16a.dat"
+# doesnt work with parsing: link = "https://coral.ise.lehigh.edu/wp-content/uploads/2014/07/data.d/chr12a.dat"
+
+# parse data from dat file
+n = pd.read_csv(link, sep=" ", header=None, nrows=1)[0][0]
+m = n**2 + 1 # final size after expanding to R^{nxn+1}
+df = pd.read_csv(link, sep=" ", header=None, skiprows=1)
+A = df.iloc[:n].to_numpy(dtype=np.float32)
+B = df.iloc[n:].to_numpy(dtype=np.float32)
+# C = 0 for pure QAP
+
+############# PSD Variable ############################
+Y = cp.Variable((m,m), PSD=True)
+
+############# Objective Function ######################
+# Lq = [[0, 0], [0, B kron A]]
+# Y = [[1, x^T], [x, xx^T]] -- will find with PSD solver
+Lq = np.kron(B, A)
+Lq = np.r_[[[0]*(m-1)], Lq]
+Lq = np.c_[[0]*m, Lq] # Lq
+
+###################### Constraints ############################
+I = np.identity(n) # identity
+E = np.ones(shape=(n,n)) # ones matrix
+D = np.kron(I, E) + np.kron(E, I)
+D = np.r_[[[-2]*(m-1)], D]
+D = np.c_[[-2]*m, D]
+D[0,0] = 2*n # D
+
+# actual constraints
+constraints = [Y[0][0] == 1]
+constraints += [cp.diag(Y) - Y[:,0] == np.zeros(m)]
+constraints += [cp.sum([Y[i:i+n, i:i+n] for i in range(1,m,n)]) == I]
+constraints += [cp.sum([Y[i:m:n, i:m:n] for i in range(1,n+1)]) == I]
+constraints += [cp.trace(D @ Y) == 0]
+
+################# Objective & Solve ####################\
+PSDprob = cp.Problem(cp.Minimize(cp.trace(Lq @ Y)), constraints) # minimize tr(LqY) subject to the constraints
+PSDprob.solve(verbose=True)
+
+# Print results
+print("The optimal value is ", PSDprob.value)
+print("A solution Y is")
+print(Y.value)
